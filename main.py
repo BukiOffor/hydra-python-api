82a13aa5200e7eaf1f3f74778056959848d040a9
Ebuka <ebuka2264@yahoo.com>
2023-11-15 21:54:52 +0100

    wallet data

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import iop_python as iop
+import asyncio
 
+
+async def get_wallet(wallet):
+    await iop.call_wallet(wallet)
+
+
+
+main_wallet = "blind market ability shoot topple round inmate pass lunch symbol average alpha party notice switch sea pass toy alien fuel pull angle weather scan"
 seedPhrase = iop.generate_phrase()
-wallet = iop.get_wallet(seedPhrase)
-print(wallet)
+#print(seedPhrase)
+ark_wallet = iop.get_ark_wallet(main_wallet)
+hyd_wallet = iop.get_wallet(main_wallet)
+print("ark wallet: ",ark_wallet)
+print('hyd wallet: ',hyd_wallet)
+asyncio.run(get_wallet("tfzy3zXYUuEPoX17GkJzQnavdbRmmmFLk6"))
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,41 @@
+use std::{error::Error, fmt::Display};
+use async_std::task;
+use serde::{Deserialize, Serialize};
+
+
+
+
+use reqwest::{get, Url};
+
+
+
 use pyo3::prelude::*;
-use iop_sdk::{vault::{Bip39, Vault, hydra}, ciphersuite::secp256k1::{hyd,SecpPrivateKey,SecpPublicKey,SecpKeyId}};
+use iop_sdk::{vault::{Bip39, Vault, hydra, PrivateKey, Network}, ciphersuite::secp256k1::{hyd,SecpPrivateKey,SecpPublicKey,SecpKeyId}};
 use iop_sdk::hydra::txtype::{OptionalTransactionFields,CommonTransactionFields};
 use iop_sdk::hydra::txtype::{Aip29Transaction,hyd_core::Transaction};
+#[derive(Debug)]
+pub enum Err{
+    CouldNotSendTrsansaction
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
+pub struct Data {
+    address:String,
+    nonce:String,
+    balance:String,
+    isDelegate:bool,
+    isResigned:bool
+    }
 
-//use std::error::Error;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Response {data: Data}
 
+impl Display for Err{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+impl Error for Err{}
 
 #[allow(unused_variables)]
 pub fn send_tx(
@@ -23,12 +54,35 @@ pub fn send_tx(
     Ok(res.get_id().expect("Something went wrong with the transaction"))
 
 }
+#[allow(unused_variables)]
+pub async fn send_transaction(phrase:String,receiver:&str,amount:u64) -> Result<String, Box< dyn Error>>{
+    //let (signer,public_key, key_id) = get_keys(phrase).unwrap();
+    let signer = get_keys(phrase).unwrap();
+    let recipient_id = SecpKeyId::from_p2pkh_addr(receiver, &hyd::Testnet).unwrap();
+
+    Err(Box::new(Err::CouldNotSendTrsansaction))
+}
+async fn get_wallet_data<'a>(addr:String ){ 
+    task::spawn(async move {
+    let url = Url::parse("https://test.explorer.hydraledger.io:4705/api/v2/")
+        .unwrap()
+        .join(&format!("wallets/{addr}")).unwrap();
+    let response = get(url).await.unwrap();
+    if response.status().is_success(){
+        let body = response.text().await.unwrap();
+        let input = format!(r#"{body}"#);
+        let object: Response = serde_json::from_str(&input).unwrap();
+        let address = object.data.address;        
+        println!("Response body: {}", address);
+    }
+}).await; 
+    
+} 
 
 #[pyfunction]
 pub fn generate_phrase() ->PyResult<String>{
     let bip = Bip39::new();
     let phrase = bip.generate().as_phrase().to_owned();
-    //println!("{}", phrase);
     Ok(phrase)
 }
 
@@ -42,11 +96,23 @@ pub fn get_wallet(phrase: String) -> PyResult<String> {
     let wallet_private = wallet.private("password");
     let wallet_address = wallet.public()
         .expect("ERROR while unwrapping public key")
-        .key(0).expect("Error while getting wallet Address").to_p2pkh_addr();
+        .key_mut(0).expect("Error while getting wallet Address").to_p2pkh_addr();
+
     Ok(wallet_address)
 }
+#[pyfunction]
+#[allow(unused_variables)]
+pub fn get_ark_wallet(phrase:String) -> PyResult<String> {
+    let network = &hyd::Testnet;
+    let ark_pk = SecpPrivateKey::from_ark_passphrase(phrase).unwrap();
+    let ark_kpub = ark_pk.public_key();
+    let ark_key_id = ark_kpub.ark_key_id();
+    let ark_address = ark_key_id.to_p2pkh_addr(network.p2pkh_addr());
+    Ok(ark_address)
+}
 
-pub fn get_signer(phrase: String) -> Result<SecpPrivateKey,()> {
+
+fn get_keys(phrase: String) -> Result<(SecpPrivateKey,SecpPublicKey,SecpKeyId),()> {
     let mut vault = Vault::create(None, phrase, "password", "password").expect("Vault could not be initialised");
     let params = hydra::Parameters::new(&hyd::Testnet,0);
     hydra::Plugin::init(&mut vault, "password", &params).expect("plugin could not be initialised");
@@ -54,11 +120,21 @@ pub fn get_signer(phrase: String) -> Result<SecpPrivateKey,()> {
     let wallet_private = wallet.private("password")
         .expect("private struct could not be unwrapped")
         .key(0)
-        .expect("private key could not be unwrapped").to_private_key();    
-    Ok(wallet_private)
+        .expect("private key could not be unwrapped").to_private_key();  
+    let wallet_public = wallet.public().unwrap().key(0).unwrap().to_public_key(); 
+    let wallet_key_id = wallet.public().unwrap().key(0).unwrap().to_key_id();
+    Ok((wallet_private,wallet_public,wallet_key_id))
 }
 
 
+
+#[pyfunction]
+pub fn call_wallet(py: Python, addr:String) -> PyResult<&PyAny> {
+    pyo3_asyncio::async_std::future_into_py(py, async move {
+        get_wallet_data(addr).await;
+        Ok(())
+    })
+}
 
 
 /// A Python module implemented in Rust.
@@ -66,6 +142,11 @@ pub fn get_signer(phrase: String) -> Result<SecpPrivateKey,()> {
 fn iop_python(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(generate_phrase, m)?)?;
     m.add_function(wrap_pyfunction!(get_wallet, m)?)?;
+    m.add_function(wrap_pyfunction!(get_ark_wallet, m)?)?;
+    m.add_function(wrap_pyfunction!(call_wallet, m)?)?;
+
+    
+
 
     Ok(())
 }
