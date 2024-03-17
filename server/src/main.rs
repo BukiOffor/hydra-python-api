45765664845f05a2d413d9945fecc9c643d03a09
BukiOffor <ebuka2264@yahoo.com>
2024-03-17 17:47:45 +0100

    feat: server & build

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,7 +6,7 @@ use handlers::api::{
     generate_did_by_morpheus,sign_witness_statement,
     verify_signed_statement,generate_nonce,get_morpheus_vault,
     get_hyd_vault, get_new_acc_on_vault,
-    // validate_statement_with_did,
+    validate_statement_with_did,
 };
 use log::LevelFilter;
 
@@ -16,7 +16,7 @@ use log::LevelFilter;
 async fn main() -> std::io::Result<()> {
     env_logger::builder().filter_level(LevelFilter::Info).init();
     log::info!("server initialized and running at port 8088");
-    log::info!("Running server at http://127.0.0.1:8088");
+    log::info!("Running server at http://0.0.0.0:8088");
     HttpServer::new(move || {        
         App::new()    
             .service(generate_phrase)
@@ -30,9 +30,9 @@ async fn main() -> std::io::Result<()> {
             .service(get_morpheus_vault)
             .service(get_hyd_vault)
             .service(get_new_acc_on_vault)
-            // .service(validate_statement_with_did)        
+            .service(validate_statement_with_did)        
         })
-        .bind("127.0.0.1:8088")?
+        .bind("0.0.0.0:8088")?
         .run()
         .await
 }
