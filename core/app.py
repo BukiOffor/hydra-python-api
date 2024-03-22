e23f1c1ff9811af712cd4e8a0084c967da3c8881
BukiOffor <ebuka2264@yahoo.com>
2024-03-22 12:09:05 +0100

    build server with docker

--- core/app.py
+++ core/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import iop_python as iop
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return iop.generate_phrase()
+
+if __name__ == '__main__':
+    app.run()
