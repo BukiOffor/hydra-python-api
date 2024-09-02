32f7abe1d84ececa46d199d7ce3a9d26090ed6ac
BukiOffor <ebuka2264@yahoo.com>
2024-09-02 21:01:56 +0100

    switch

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ mywallet = hydra.HydraWallet()
 
 phrase = "blind market ability shoot topple round inmate pass lunch symbol average alpha party notice switch sea pass toy alien fuel pull angle weather scan"
 wallet = iop.get_morpheus_vault(phrase,"pass")
-
+mainnet_phrase="effort alarm inject boss spin ramp barely evolve expand across hawk capital village traffic sure trap basic cross volume keep fantasy bulk diesel cool"
 
 
 
@@ -18,7 +18,7 @@ password="horse-staple-God"
 data = iop.get_hyd_vault(mainnet_phrase,password,"mainnet")
 wallet = iop.get_wallet(data,0,"mainnet")
 print(wallet) # hVKiVHyKFzsm2MdKR2NxBCs3K8EoKGz3Dq
-response = iop.generate_transaction(data,"hMU5sH5D91anf8WNKEvcF4pW7naeijffUB",50000000,2,password,0,"mainnet") # send 0.5
+response = iop.generate_transaction(data,"hGfbtvLCZV8QT29dHbupLdhndhfTRdFW9W",50000000,7,password,0,"mainnet") # send 0.5
 url = "http://explorer.hydraledger.tech:4703/api/v2/transactions"
 res = requests.post(url, json=json.loads(response))
 response = res.json()
